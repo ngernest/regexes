fe85128fcf34ae116551c0eac058237282802889
Ernest Ng <eyn5@cornell.edu>
2024-11-02 14:31:06 -0400

    single quote notation for chars

--- coq/filinski.v
+++ coq/filinski.v
@@ -1,8 +1,21 @@
-Require Import List Bool.
+Require Import List Bool Ascii String.
 Import ListNotations.
+Open Scope list_scope.
+Open Scope string_scope.
+Open Scope char_scope.
+
+(* Some notation so that we can use single quotes to represent chars
+   https://stackoverflow.com/questions/26112349/single-quote-notation-for-characters-in-coq *)
+
+Notation "''c''" := "c" : char_scope.
+Notation "''a''" := "a" : char_scope.
+
+(* Note that the literal ['a'] has type [ascii], aka [char] *)
+Check 'a'.
 
 (* Polymorphic regular expressions: regexes in [re T] describe 
-   strings with characters drawn from [T], i.e. lists of elements of [T] *)
+   strings with characters drawn from [T], i.e. lists of elements of [T]. 
+   (Adapted from HW3) *)
 Inductive re (T : Type) : Type :=
   | Void
   | Eps
@@ -10,4 +23,5 @@ Inductive re (T : Type) : Type :=
   | Seq (r1 r2 : re T)
   | Alt (r1 r2 : re T)
   | Star (r : re T). 
+
 
