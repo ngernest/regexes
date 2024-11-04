3c66567bad2da348e5bb63c746e0862fc03c80d2
Laura Zielinski <lczielinski1@gmail.com>
2024-11-04 14:58:09 -0500

    started using different notation. proved a_der_weakening

--- coq/Antimirov.v
+++ coq/Antimirov.v
@@ -8,13 +8,13 @@ Generalizable All Variables.
 (* Antimirov derivative of a regex [re] with respect to a char [a] *)
 Fixpoint a_der (r : re) (a : char) : gset re :=
   match r with
-  | Void => gset_empty
-  | Epsilon => gset_empty
-  | Atom b => if char_dec a b then gset_singleton Epsilon else gset_empty
-  | Union r1 r2 => gset_union (a_der r1 a) (a_der r2 a)
+  | Void => ∅
+  | Epsilon => ∅
+  | Atom b => if char_dec a b then {[ Epsilon ]} else ∅
+  | Union r1 r2 => (a_der r1 a) ∪ (a_der r2 a)
   | Concat r1 r2 => 
     if isEmpty r1 
-      then gset_union (set_map (fun r => Concat r r2) (a_der r1 a)) (a_der r2 a) 
+      then (set_map (fun r => Concat r r2) (a_der r1 a)) ∪ (a_der r2 a) 
     else (set_map (fun r => Concat r r2) (a_der r1 a))
   | Star r => set_map (fun r' => Concat r' (Star r)) (a_der r a)
   end.
@@ -30,28 +30,22 @@ Definition aderiv (c : char) (rs : gset re) : gset re :=
        then [s] matches the union of [rs] with any other singleton regex set *)
 Inductive matches_set : string -> gset re -> Prop :=
   | matches_set_here (r : re) (s : string) (rs : gset re) : 
-      matches r s -> 
-      gset_elem_of r rs -> 
+      matches r s -> r ∈ rs -> 
       matches_set s rs
   | matches_set_there (r : re) (s : string) (rs : gset re) : 
       matches_set s rs -> 
-      matches_set s (gset_union (gset_singleton r) rs).
-
-Lemma gset_union_assoc `{Countable A} (X Y Z : gset A) :
-  mapset.mapset_union (mapset.mapset_union X Y) Z = mapset.mapset_union X (mapset.mapset_union Y Z).
-Proof. Search (mapset.mapset_union). Admitted.
+      matches_set s ({[ r ]} ∪ rs).
 
 (* Adapted from https://monog.ufop.br/server/api/core/bitstreams/d7d18cf6-ff09-4b32-99a6-d87235f7a3ce/content *)
 Lemma a_der_weakening : forall (rs rs' : gset re) (s : string),
   matches_set s rs -> 
-  matches_set s (gset_union rs rs').
+  matches_set s (rs ∪ rs').
 Proof.
-  intros. inv' H.
-  - eapply matches_set_here. apply H0. 
-    eremember elem_of_union_l. eapply e in H1. 
-    unfold union in H1. apply H1.
-    Unshelve. apply gset_empty. apply gset_singleton. apply gset_semi_set.
-  - Search (matches_set).
-    unfold gset_union. 
-    Locate "∪". 
-Admitted.
+  intros. induction H.
+  - eapply matches_set_here. apply H. 
+    apply elem_of_union_l. apply H0.
+  - replace ({[r]} ∪ rs ∪ rs') with ({[r]} ∪ (rs' ∪ rs)) by set_solver.
+    eapply matches_set_there.
+    replace (rs' ∪ rs) with (rs ∪ rs') by set_solver.
+    apply IHmatches_set.
+Qed.
--- coq/Brzozowski.v
+++ coq/Brzozowski.v
@@ -38,4 +38,12 @@ Proof. unfold matches'. split; revert r; induction s; X. Qed.
   - Apply simplification rules to regexes, prove that b_derivatives only 
     generate finitely many regexes up to the simplification rules 
     (so that you could build a DFA with it)
+
+   For the extensions I suggested above, here are some challenges you might encounter:
+Other logical operators such as negation: how do you reformulate the matches relation? Coq will reject a naive definition due to a non-positive recursive occurrence
+Weighted version: it'd be neat to have a version with star semirings, but how do you specify the correctness in that case?
+Antimirov derivatives: I think this should be fairly straightforward. It would also work with the weighted version
+Simplification rules: some care has to be taken to convince Coq of the termination of the simplification. In order to generate a finite automaton, you'd have to incorporate commutativity & associativity of +. You could also define a representation with an n-ary +, with the list kept in sorted & deduplicated order
+In general, it would be neat to have a regex matcher that can be extracted to ocaml code (using Coq's Extraction), and is reasonably efficient
+Another extension that would be neat is generating a minimal automaton. Or a compiler that outputs Iris-VST verified C code. But those would be more difficult :slightly_smiling_face:
 *)
