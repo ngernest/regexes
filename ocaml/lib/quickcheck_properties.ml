df770da048a966fd40101e7c22edaa18dbce3766
Ernest Ng <eyn5@cornell.edu>
2024-11-12 22:18:47 -0500

    QC generator for strings that match a regex

--- ocaml/lib/quickcheck_properties.ml
+++ ocaml/lib/quickcheck_properties.ml
@@ -13,6 +13,37 @@ open Sexplib.Conv
 let gen_re_char : (re * char) Generator.t = 
   Generator.both quickcheck_generator_re Generator.char_lowercase
 
+(** Returns a generator that produces random strings matching the regex [r] 
+    (if such a generator exists) *)  
+let rec gen_regex_string (r : re) : (string Generator.t) option = 
+  let open Generator in 
+  let open Generator.Let_syntax in 
+  match r with 
+  | Void -> None
+  | Epsilon -> Some (return "")
+  | Char c -> Some (return (Base.Char.to_string c))
+  | Alt (r1, r2) -> 
+    begin match (gen_regex_string r1, gen_regex_string r2) with 
+    | None, None -> None
+    | Some g1, _ -> Some g1 
+    | _, Some g2 -> Some g2
+    end 
+  | Seq (r1, r2) -> 
+    begin match (gen_regex_string r1, gen_regex_string r2) with 
+    | Some g1, Some g2 -> 
+      Some (let%map s1 = g1 and s2 = g2 in s1 ^ s2)
+    | _ -> None
+    end 
+  | Star r' -> 
+    begin match gen_regex_string r' with 
+    | None -> None 
+    | Some g -> Some (
+      (* iterate at most k times *)
+      let%bind k = small_strictly_positive_int in 
+      let%bind xs = list_with_length ~length:k g in  
+      return @@ String.concat "" xs)
+    end
+
 (** Only generates pairs of regexes and chars for which the set of 
   Antimirov derivatives is non-empty *)  
 let gen_re_char_nonempty_antimirov : (re * char) Generator.t = 
