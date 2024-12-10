d45020527866958c0eb90b3b9edec8b1e646d64c
Ernest Ng <eyn5@cornell.edu>
2024-12-10 14:45:14 -0500

    Gset versions of unfocus + derive_word for zippers

--- coq/EdelmannGset.v
+++ coq/EdelmannGset.v
@@ -37,10 +37,23 @@ Definition zipper_add (ctx : context) (z : zipper) : zipper :=
 (* Convert a regular expression into a zipper. *)
 Definition focus (e: re) : zipper := singleton [e].
 
+(* Typeclass instance needed to make the definition of [unfocus] below 
+   typecheck *)
+Instance ContextElements : Elements re context := {
+  elements := fun ctx => ctx
+}.
+
+(* Conversion from zipper back to re. 
+ * Unused, but provides some intuition on zippers.
+ *)
+Definition unfocus (z : zipper) : re :=
+  let ds := set_map (fun ctx => set_fold Concat Epsilon ctx) z in
+  set_fold Regex.Union Void (ds : gset re).
+
 (***** DERIVATION *****)
 
 (* Does the expression e accept the empty string? *)
-Fixpoint nullable (e: re) : bool :=
+Fixpoint nullable (e : re) : bool :=
   match e with
   | Void => false
   | Epsilon => true
@@ -65,7 +78,7 @@ Fixpoint derive_down (c : char) (e : re) (ctx : context) : zipper :=
   end.
 
 (* Upwards phase of Brzozowski's derivation on zippers. *)
-Fixpoint derive_up (c: char) (ctx: context): zipper :=
+Fixpoint derive_up (c : char) (ctx : context) : zipper :=
   match ctx with
   | [] => ∅ 
   | e :: ctx' => if nullable e
@@ -75,6 +88,9 @@ Fixpoint derive_up (c: char) (ctx: context): zipper :=
       derive_down c e ctx'
   end.
 
+
+(* Some typeclass instances needed to make the definition of [derive]
+   below typecheck *)
 Instance ZipperElements : Elements zipper zipper := {
   elements := fun z => [z]
 }. 
@@ -83,11 +99,21 @@ Instance ZipperSingleton : Singleton zipper zipper := {
   singleton := fun z => z
 }.
 
-(* Brzozowski's derivation on zippers. *)
+(* Brzozowski derivatives for zippers *)
 Definition derive (c : char) (z : zipper) : zipper :=
-  set_fold zipper_union (∅ : zipper) 
-    ((set_map (fun ctx => derive_up c ctx) z) : zipper).
+  set_fold zipper_union ∅  
+    (set_map (derive_up c) z : zipper).
 
-(* TODO: test whether this definition of derive actually works *)    
+(* Generalisation of derivatives to words. *)
+Fixpoint derive_word (w : word) (z : zipper) : zipper :=
+  match w with
+  | [] => z
+  | c :: w' => derive_word w' (derive c z)
+  end.
+
+(* Checks if the zipper z accept the empty word
+   - TODO: find the [stdpp] equivalents of [existsb] and [forallb] *)
+(* Definition zipper_nullable (z : zipper) : bool :=
+  existsb (fun ctx => forallb nullable ctx) z. *)
 
 (* TODO: continue porting the rest of Edelmann.v to gsets *)
