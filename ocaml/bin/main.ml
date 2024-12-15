eb5aae72aa20a02fa8ab4ada2585bf6ecbc90fa4
Ernest Ng <eyn5@cornell.edu>
2024-12-15 11:21:04 -0500

    Demo with three matchers

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -1,5 +1,8 @@
-open Lib
-open Zipper_antimirov
+(* open Lib.Zipper_antimirov *)
 
-let () = zipper_antimirov_demo ()
+(* let () = zipper_antimirov_demo () *)
+
+open Lib.Three_matchers
+
+let () = three_matchers_demo ()
 
---
+++ ocaml/lib/three_matchers.ml
@@ -0,0 +1,25 @@
+open Brzozowski
+open Antimirov
+open Extracted_brzozowski_zipper
+open Quickcheck_properties
+open Utils
+open Core
+
+let mk_accept_string (b : bool) : string = 
+  if b then "accepted" else "rejected"
+
+let three_matchers_demo () = 
+  Quickcheck.iter
+    ~seed:(`Deterministic "hello")
+    ~trials:15
+    gen_re_string 
+    ~f:(fun (r, s) -> 
+      printf "r = %s\ns = \"%s\"\n" (string_of_re r) s;
+      let brzozowski_res = brzozowski_match r s in 
+      let antimirov_res = antimirov_match r s in 
+      let zipper_res = zipper_match r s in 
+      printf "Brzozowski matcher : %s\n" (mk_accept_string brzozowski_res);
+      printf "Antimirov matcher  : %s\n" (mk_accept_string antimirov_res);
+      printf "Zipper matcher     : %s\n" (mk_accept_string zipper_res);
+      printf "***************************************\n")
+
